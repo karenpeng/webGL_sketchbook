61ad7b95b8e3c7bbce75d57d556565c22cb7ef04
Karen <karenpenglabs@gmail.com>
2014-06-26 22:59:03 -0400

    i am not learning webGL

--- webGL_js.js
+++ webGL_js.js
@@ -1,145 +1,229 @@
-// // set the scene size
-// var WIDTH = 400,
-//     HEIGHT = 300;
+// // // set the scene size
+// // var WIDTH = 400,
+// //     HEIGHT = 300;
 
-// // set some camera attributes
-// var VIEW_ANGLE = 45,
-//     ASPECT = WIDTH / HEIGHT,
-//     NEAR = 0.1,
-//     FAR = 10000;
+// // // set some camera attributes
+// // var VIEW_ANGLE = 45,
+// //     ASPECT = WIDTH / HEIGHT,
+// //     NEAR = 0.1,
+// //     FAR = 10000;
 
-// // get the DOM element to attach to
-// // - assume we've got jQuery to hand
-// var $container = $('#container');
+// // // get the DOM element to attach to
+// // // - assume we've got jQuery to hand
+// // var $container = $('#container');
 
-// var camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
-// camera.position.z = 500;
+// // var camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
+// // camera.position.z = 500;
 
-// var renderer = new THREE.WebGLRenderer();
-// renderer.setSize( window.innerWidth, window.innerHeight );
+// // var renderer = new THREE.WebGLRenderer();
+// // renderer.setSize( window.innerWidth, window.innerHeight );
 
-// var scene = new THREE.Scene();
-// renderer.setSize(WIDTH, HEIGHT);
+// // var scene = new THREE.Scene();
+// // renderer.setSize(WIDTH, HEIGHT);
 
-// $container.append(renderer.domElement);
+// // $container.append(renderer.domElement);
 
-// // set up the sphere vars
-// var radius = 50, segments = 16, rings = 16;
+// // // set up the sphere vars
+// // var radius = 50, segments = 16, rings = 16;
 
-// // create a new mesh with sphere geometry -
-// // we will cover the sphereMaterial next!
-// var sphere = new THREE.Mesh(
-//    new THREE.SphereGeometry(radius,
-//    segments,
-//    rings),
+// // // create a new mesh with sphere geometry -
+// // // we will cover the sphereMaterial next!
+// // var sphere = new THREE.Mesh(
+// //    new THREE.SphereGeometry(radius,
+// //    segments,
+// //    rings),
 
-//    sphereMaterial);
+// //    sphereMaterial);
 
-// // add the sphere to the scene
-// scene.add(sphere);
+// // // add the sphere to the scene
+// // scene.add(sphere);
 
-// // create the sphere's material
-// var sphereMaterial = new THREE.MeshLambertMaterial(
-// {
-//   // a gorgeous red.
-//   color: 0xCC0000
-// });
+// // // create the sphere's material
+// // var sphereMaterial = new THREE.MeshLambertMaterial(
+// // {
+// //   // a gorgeous red.
+// //   color: 0xCC0000
+// // });
 
-// // create a point light
-// var pointLight = new THREE.PointLight( 0xFFFFFF );
+// // // create a point light
+// // var pointLight = new THREE.PointLight( 0xFFFFFF );
 
-// // set its position
-// pointLight.position.x = 10;
-// pointLight.position.y = 50;
-// pointLight.position.z = 130;
+// // // set its position
+// // pointLight.position.x = 10;
+// // pointLight.position.y = 50;
+// // pointLight.position.z = 130;
 
-// // add to the scene
-// scene.add(pointLight);
-
-// renderer.render( scene, camera );
-
-// // function render(){
-// // requestAnimationFrame( render );
-
-// // cube.rotation.x += 0.1;
-// // cube.rotation.y += 0.1;
+// // // add to the scene
+// // scene.add(pointLight);
 
 // // renderer.render( scene, camera );
+
+// // // function render(){
+// // // requestAnimationFrame( render );
+
+// // // cube.rotation.x += 0.1;
+// // // cube.rotation.y += 0.1;
+
+// // // renderer.render( scene, camera );
+// // // }
+// // // render();
+
+// //Get A WebGL context
+// // var canvas = document.getElementById("myCanvas");
+// // var gl = canvas.getContext("experimental-webgl");
+
+// // // setup a GLSL program
+// // var vertexShader = createShaderFromScriptElement(gl, "2d-vertex-shader");
+// // var fragmentShader = createShaderFromScriptElement(gl, "2d-fragment-shader");
+// // var program = createProgram(gl, [vertexShader, fragmentShader]);
+// // gl.useProgram(program);
+
+// // // look up where the vertex data needs to go.
+// // var positionLocation = gl.getAttribLocation(program, "a_position");
+
+// // // Create a buffer and put a single clipspace rectangle in
+// // // it (2 triangles)
+// // var buffer = gl.createBuffer();
+// // gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+// // gl.bufferData(
+// //   gl.ARRAY_BUFFER,
+// //   new Float32Array([-1.0, -1.0,
+// //     1.0, -1.0, -1.0, 1.0, -1.0, 1.0,
+// //     1.0, -1.0,
+// //     1.0, 1.0
+// //   ]),
+// //   gl.STATIC_DRAW);
+// // gl.enableVertexAttribArray(positionLocation);
+// // gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+
+// // // draw
+// // gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+// // var gl; // A global variable for the WebGL context
+
+// // function start() {
+// //   var canvas = document.getElementById("myCanvas");
+
+// //   gl = initWebGL(canvas); // Initialize the GL context
+
+// //   // Only continue if WebGL is available and working
+
+// //   if (gl) {
+// //     gl.clearColor(0.0, 0.0, 0.0, 1.0); // Set clear color to black, fully opaque
+// //     gl.enable(gl.DEPTH_TEST); // Enable depth testing
+// //     gl.depthFunc(gl.LEQUAL); // Near things obscure far things
+// //     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); // Clear the color as well as the depth buffer.
+// //   }
 // // }
-// // render();
 
-//Get A WebGL context
-// var canvas = document.getElementById("myCanvas");
-// var gl = canvas.getContext("experimental-webgl");
+// var scene = new THREE.Scene();
+// var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight,
+//   0.1, 1000);
 
-// // setup a GLSL program
-// var vertexShader = createShaderFromScriptElement(gl, "2d-vertex-shader");
-// var fragmentShader = createShaderFromScriptElement(gl, "2d-fragment-shader");
-// var program = createProgram(gl, [vertexShader, fragmentShader]);
-// gl.useProgram(program);
+// var renderer = new THREE.WebGLRenderer();
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// document.body.appendChild(renderer.domElement);
 
-// // look up where the vertex data needs to go.
-// var positionLocation = gl.getAttribLocation(program, "a_position");
+// var geometry = new THREE.BoxGeometry(1, 1, 1);
+// var material = new THREE.MeshBasicMaterial({
+//   color: 0x00ff00
+// });
+// var cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
 
-// // Create a buffer and put a single clipspace rectangle in
-// // it (2 triangles)
-// var buffer = gl.createBuffer();
-// gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-// gl.bufferData(
-//   gl.ARRAY_BUFFER,
-//   new Float32Array([-1.0, -1.0,
-//     1.0, -1.0, -1.0, 1.0, -1.0, 1.0,
-//     1.0, -1.0,
-//     1.0, 1.0
-//   ]),
-//   gl.STATIC_DRAW);
-// gl.enableVertexAttribArray(positionLocation);
-// gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+// camera.position.z = 5;
 
-// // draw
-// gl.drawArrays(gl.TRIANGLES, 0, 6);
+// var render = function () {
+//   requestAnimationFrame(render);
 
-// var gl; // A global variable for the WebGL context
+//   cube.rotation.x += 0.1;
+//   cube.rotation.y += 0.1;
 
-// function start() {
-//   var canvas = document.getElementById("myCanvas");
+//   renderer.render(scene, camera);
+// };
 
-//   gl = initWebGL(canvas); // Initialize the GL context
+// render();
 
-//   // Only continue if WebGL is available and working
+$(document).ready(function () {
+  init()
+})
 
-//   if (gl) {
-//     gl.clearColor(0.0, 0.0, 0.0, 1.0); // Set clear color to black, fully opaque
-//     gl.enable(gl.DEPTH_TEST); // Enable depth testing
-//     gl.depthFunc(gl.LEQUAL); // Near things obscure far things
-//     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); // Clear the color as well as the depth buffer.
-//   }
-// }
+function init() {
+  var glLib = GlLib("#canvas");
+  var canvas = glLib.canvas;
+  canvas.width(Math.min(canvas.width(), 1024));
+  canvas.height(Math.min(canvas.height(), 1024));
+  canvas.mousemove(mousemove);
+  var gl = glLib.gl;
+  gl.disable(gl.CULL_FACE);
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight,
-  0.1, 1000);
+  var mx = -1000,
+    my = -1000;
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+  var drawShader = new glLib.shader("#vs", "#fs_draw");
+  var renderShader = new glLib.shader("#vs", "#fs_render");
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({
-  color: 0x00ff00
-});
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+  var fboW = 1024,
+    fboH = 1024;
+  var fbos = [new glLib.fbo(fboW, fboH), new glLib.fbo(fboW, fboH)];
 
-camera.position.z = 5;
+  fbos[0].bind();
+  glLib.clear(0.95, 0.95, 0.95, 1);
+  fbos[0].unbind();
+  fbos[1].bind();
+  glLib.clear(0.95, 0.95, 0.95, 1);
+  fbos[1].unbind();
 
-var render = function () {
-  requestAnimationFrame(render);
+  var plane = new glLib.bo();
+  var sp = new shape.Plane(2, 2);
+  plane.addVbo("position", sp.vertexPositions, 3);
+  plane.setIbo(sp.indices);
 
-  cube.rotation.x += 0.1;
-  cube.rotation.y += 0.1;
+  var ovMat4 = mat4.lookAt(mat4.create(), [0.0, 0.0, 0.5], [0.0, 0.0, 0.0], [0,
+    1, 0
+  ]);
+  var opMat4 = mat4.ortho(mat4.create(), -1.0, 1.0, 1.0, -1.0, 0.1, 1)
+  var ortho = glLib.mvp(mat4.create(), ovMat4, opMat4);
 
-  renderer.render(scene, camera);
+  update();
+  draw();
+  glLib.setIteration(update, draw, 60);
+
+  //--------------------------------------------
+  function update() {
+    swap();
+    drawShader.setUniform2fv('size', [canvas.width(), canvas.height()]);
+    drawShader.setUniform2fv('mousepos', [mx, my]);
+    drawShader.setUniformM4fv('mvpMatrix', ortho);
+    drawShader.setUniform1f('t', glLib.getFrameNum() * 0.02);
+
+    renderShader.setUniform2fv('size', [canvas.width(), canvas.height()]);
+    renderShader.setUniform2fv('mousepos', [mx, my]);
+    renderShader.setUniformM4fv('mvpMatrix', ortho);
+  }
+
+  //--------------------------------------------
+  function draw() {
+    fbos[0].bind();
+    fbos[1].textureBind(drawShader, "texture", 0);
+    plane.draw(drawShader);
+    fbos[0].unbind();
+
+    fbos[0].textureBind(renderShader, "texture", 0);
+    plane.draw(renderShader);
+    glLib.flush();
+  }
+
+  //--------------------------------------------
+  function mousemove(evt) {
+    mx = evt.offsetX;
+    my = canvas.height() - evt.offsetY;
+  }
+
+  //--------------------------------------------
+  function swap() {
+    var t = fbos[0];
+    fbos[0] = fbos[1];
+    fbos[1] = t;
+  }
 };
-
-render();
